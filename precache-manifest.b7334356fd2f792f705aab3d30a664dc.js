cff62e6c93128654b00472164b0473c1c60d4264
Glen Pegado <glenpegado@gmail.com>
2019-09-04 14:48:36 -0500

    Updates

---
+++ precache-manifest.b7334356fd2f792f705aab3d30a664dc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b5efb8b4130334e2b0f1e0cf143dab6",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2dcac1b6f95b6ab4a419",
+    "url": "/monsters-rolodex/static/css/main.3502a5d0.chunk.css"
+  },
+  {
+    "revision": "d81979aa8ac68af22713",
+    "url": "/monsters-rolodex/static/js/2.4ea1ec15.chunk.js"
+  },
+  {
+    "revision": "2dcac1b6f95b6ab4a419",
+    "url": "/monsters-rolodex/static/js/main.9fcbe6b9.chunk.js"
+  },
+  {
+    "revision": "c7f991e6cb4746d6bd30",
+    "url": "/monsters-rolodex/static/js/runtime~main.205b164d.js"
+  }
+]);
