c9832a970a3fbe89d67f35c1fd8bc980adcf0eae
Glen Pegado <glenpegado@gmail.com>
2019-09-05 11:47:47 -0500

    Added final files

--- src/App.js
+++ src/App.js
@@ -24,17 +24,20 @@ class App extends React.Component{
 
   render() {
     const { monsters, searchField } = this.state;
+
     const filteredMonsters = monsters.filter(monster =>
-        monster.name.toLowerCase().includes(searchField.toLowerCase())
-    );
+        monster.name.toLowerCase().includes(searchField.toLowerCase()));
 
     return (
       <div className='App'>
-        <SearchBox 
-          placeholder='search monsters'
-          handleChange={this.handleChange}
-        />
-        <CardList monsters={filteredMonsters}/> 
+          <h1>Monsters Rolodex</h1>
+          <SearchBox 
+            placeholder='search monsters'
+            handleChange={this.handleChange}
+          />
+
+          <CardList monsters={filteredMonsters}/> 
+
       </div>
     )
   }
