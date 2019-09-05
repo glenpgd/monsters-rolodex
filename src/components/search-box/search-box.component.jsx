0867ad4a6ab372ae0b179b91f6a0bd353427272e
Glen Pegado <glenpegado@gmail.com>
2019-09-05 10:30:05 -0500

    included handleChange function inside of App.js

--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -3,9 +3,9 @@ import './search-box.styles.css';
 
 export const SearchBox = ({placeholder, handleChange}) => (
     <input 
-    className='search'
-    type='search' 
-    placeholder={placeholder} 
-    onChange={handleChange}
-  />
+        className='search'
+        type='search' 
+        placeholder={placeholder} 
+        onChange={handleChange}
+    />
 )
